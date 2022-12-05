919423cac30074842e9a39e7c6fe6d304893067e
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2022-12-04 20:09:56 -0500

    Update Passgen.py

--- Passgen.py
+++ Passgen.py
@@ -1,11 +1,9 @@
 # Requires string, secrets, and pyperclip
 import secrets
 import string as txt
-import pyperclip as clipboard
 chrs = txt.ascii_letters + str(1234567890)
 def genPass():
-  charCount = input("Amount of characters in password...");
-  pass = ''.join(secrets.choice(chrs))
-  print(f'Your password is: {pass}')
-  clipboard.copy(pass)
+    charCount = input("Amount of characters in password... ");
+    passW = ''.join(secrets.choice(chrs) for _ in range(int(charCount)))
+    print(f'Your password is: {passW}')
 genPass()
