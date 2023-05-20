380e9702ef2317a3cb4b3b41a0d74a721f89cca9
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2023-05-19 22:14:41 -0400

    Update concat.js

--- concat.js
+++ concat.js
@@ -4,16 +4,25 @@ class Str {
   this.info = info;
   }
   concat(arg1) {
-  var s1 = this.info.toString();
-  var s2 = arg1.toString();
-  return s1 + s2
+  var s1 = this.text.toString();
+  var s2 = arg1.text;
+  this.info = s1 + s2;
   }
-  get info() {
-  return this.info;
+  concatStr(arg1) {
+    var s1 = this.text.toString();
+    var s2 = arg1;
+    this.info = s1 + s2;
+  }
+  get text() {
+    return this.info;
   }
 }
 
-var string1 = new Str("Hello ");
-var string2 = new Str("World!");
+var string1 = new Str("Hello, ");
+var string2 = new Str("World");
+var string3 = ", This";
+var string4 = " Is a Test.";
 string1.concat(string2);
-console.log(string1);
+string1.concatStr(string3);
+string1.concatStr(string4);
+console.log(string1.info);
