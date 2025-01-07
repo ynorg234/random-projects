62cf914e45aaf5ebff02daa1a74bfbf8c254b9c5
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2025-01-06 21:55:30 -0500

    i made so much code for a one liner lollll

--- Array.js
+++ Array.js
@@ -1,24 +1,3 @@
 function array(text) {
-    var e = text;
-    if (isFinite(e) !== false) {
-        e = e.toString()
-    }
-    var a = [0];
-    var t = 0;
-    var i = 0;
-    for (var d = 0; d < e.length; d++) {
-        a[d] = 0;
-    }
-    while (i < a.length) {
-        if (i === e.length) {
-            i = 0;
-            t++;
-            if (t === e.length) {
-                break;
-            }
-        }
-        a[i] = e[i];
-        i++;
-    }
-    return a;
+    if (!isFinite(text)) {return text.toString().split("");} else {return text.toString().split("");}
 }
