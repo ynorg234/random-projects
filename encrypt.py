d64e9bd74d6395ebbffb166b49a352e8f1066086
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2025-01-06 21:56:40 -0500

    remove crappy code

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,4 @@
-from secrets import choice as sfind
-def encr():
-	text = input("Enter text to encrypt here... ")
-	etext = ""
-	stri = "abcdefghijklmnopqrstuvwxyz"
-	key = dict()
-	for i in range(len(key)):
-		key[i] = ''.join(sfind(stri) for j in range(3))
-	for k in range(len(text)):
-		if text[k] == stri[k]:
-			etext += key[k]
-	return etext
-encr()
-	
+"""
+this was ai generated
+please dont look at this
+"""	
